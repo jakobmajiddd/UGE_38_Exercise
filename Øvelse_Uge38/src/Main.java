ac7744049395099c1f52b649e40ef079b619394c
Younes Elhamss <youneselhamss@gmail.com>
2021-09-24 15:22:03 +0200

    made fb

--- Øvelse_Uge38/src/Main.java
+++ Øvelse_Uge38/src/Main.java
@@ -4,13 +4,13 @@ public class Main {
         // write your code here
         System.out.println("Hej med jer");
 
-<<<<<<< HEAD
+
         System.out.println("Make Google app");
 
         System.out.println("Nikolaj har pillet her");
         System.out.println("Nikolaj piller her også");
-        System.out.println("Make Facebook");
-=======
+
+
         JensClass jensErNice = new JensClass();
         System.out.println("Jens");
         jensErNice.jens();
@@ -18,6 +18,6 @@ public class Main {
         System.out.println("Nikolaj har pillet her");
         System.out.println("Nikolaj piller her også");
 
->>>>>>> 0316ceb12042da771a449f92cefc5c587a170b21
+        System.out.println("Make Facebook");
     }
 }
