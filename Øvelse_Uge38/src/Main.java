4405b78d1ba1946fc9d6288c8933a5fedbd0f9cd
Jens-k-m-m <jensmonteiro@gmail.com>
2021-09-24 14:57:33 +0200

    Jens push

--- Øvelse_Uge38/src/Main.java
+++ Øvelse_Uge38/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hej med jer");
+        JensClass jensErNice = new JensClass();
+        jensErNice.jens();
     }
 }
